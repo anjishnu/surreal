453c18a0b8efd27ff611058196851157a6c82582
Anjishnu Kumar <anjikum@amazon.com>
2016-02-29 03:10:43 -0800

    models are in

--- models.py
+++ models.py
@@ -4,23 +4,27 @@ from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD, Adadelta, Adagrad
 
-def convolutional_dnn(channels, img_rows, img_cols):
+def convolutional_dnn(channels, input_patch, output_patch):
+    ''' Builds a CNN model for super-resolution'''
     model = Sequential()
     model.add(Convolution2D(32, 3, 3, border_mode='same',
-                            input_shape= (channels, img_rows, img_cols)))
+                            input_shape= (channels, input_patch, input_patch)))
     model.add(Activation('relu'))
-    model.add(Convolution(32,3,3))
+    model.add(Convolution2D(32, 3, 3))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3))
     model.add(Activation('relu'))
-    model.add(Convolution2D(64, 3, 3))
-    mode.add(Activation('relu'))
     model.add(Convolution2D(128, 3, 3))
     model.add(Activation('relu'))
     model.add(Convolution2D(channels, 3, 3))
-    model.add(Dense(channels * img_rows * img_cols))    
+    model.add(Flatten())
+    model.add(Dense(channels * output_patch * output_patch))    
+    model.compile(loss='mean_squared_error',
+                  optimizer=Adagrad())    
     return model
 
 def create_model():
-    return convolutional_dnn(channels=3, img_rows=32, img_cols=32)
+    return convolutional_dnn(channels=3, input_patch=16, output_patch=32)
     
--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 from __future__ import print_function
 import keras
-import waifu2x
-import jsonpickle
-import Image as img
+from PIL import Image
 import os
+import numpy as np
 from argparse import ArgumentParser 
-from scipy.ndimage import imread as img_reader
+from scipy.ndimage import imread 
+from models import create_model
+from sklearn.feature_extraction.image import PatchExtractor
+
 
 def build_parser():
     parser = ArgumentParser()
@@ -17,42 +19,65 @@ def build_parser():
     parser.add_argument('-e', '--epochs', default=5)
     parser.add_argument('-b', '--batch_size', default=32)
     parser.add_argument('-c', '--checkpoint', default=10000)
-    parser.add_argument('-cd', '--checkpoint_directory', default='checkpoint_directory')
-    
-    return parser
+    parser.add_argument('-cd', '--checkpoint_directory',
+                        default='checkpoint_directory')    
+
+    return parser # Return the parser 
 
 
 def get_img_filepaths(directory):
+    # Traverse a directory and generate images
     for dirpath, _, filenames in os.walk(directory):
         for fname in filenames:
             yield os.path.abspath(os.path.join(dirpath, fname))
-            
 
-def generate_data(imgs_folder):
-    for fpath in get_img_filepaths(directory):        
-        print ('Reading image', fname)
-        image_tensor = img_reader(fname, mode='RGB')        
-        yield patch_list 
+            
+def generate_data(img_folder):
+
+    patch_extractor = PatchExtractor(patch_size=(32,32),
+                                     max_patches=0.01)
+    
+    def resize(img_tensor, scale=0.5):
+        # Generate a half size image tensor
+        channels, max_x, max_y = img_tensor.shape
+        img_tensor = np.rollaxis(img_tensor, axis=0, start=3)
+        image = Image.fromarray(np.asarray(img_tensor), mode='RGB')
+        small_img = np.array(image.resize(size=(int(max_x * scale),
+                                                int(max_y * scale))))
+        x_small, y_small, channels = small_img.shape
+        return np.rollaxis(small_img, axis=2)
+        
+    for fpath in get_img_filepaths(img_folder):        
+        print ('Reading image', fpath)
+        img_tensor = imread(fpath, mode='RGB')
+        input_matrix = np.array([img_tensor])
+        patches = patch_extractor.transform(input_matrix)                
+        patches = np.rollaxis(patches, axis=3, start=1)
+        small_patches = np.array([resize(patch) for patch in patches])        
+        patches = np.array([p.reshape(p.shape[0]*p.shape[1]*p.shape[2]) for p in patches])
+        print ("Shapes of tensors", small_patches.shape, patches.shape)
+        yield small_patches, patches
 
 
 def train(args=None):
-    model = waifu2x.model.create()
-    progbar = keras.utils.generic_utils.Progbar(X_train.shape[0])
+    model = create_model()
+    print ('created model')
+    total_iterations = 0
     for epoch in range(args.epochs):
-        for iteration, (train_x, train_y) in enumerate(generate_data(args.img_folder)):
+        print ('Training epoch', epoch)
+        for iteration, (train_x, train_y) in enumerate(generate_data(args.image_folder)):
+            progbar = keras.utils.generic_utils.Progbar(train_x.shape[0])    
             loss = model.train_on_batch(train_x, train_y)
-            progbar.add(X_batch.shape[0], values=[('train loss', loss[0])])
-
-            if (iteration % args.checkpoint == 0):
+            progbar.add(train_x.shape[0], values=[('train loss', loss[0])])
+            total_iterations += 1
+            if (total_iterations % args.checkpoint == 0):
+                print ('Reached checkpoint at iteration', iteration)
                 #checkpoint reached, save model and validate performance
-                val_x, val_y = get_validation(args.batchsize)
-                model.predict(val_x, val_y)										
-                test_progbar = keras.utils.generic_utils.Progbar(val_x.shape[0])
-                score = model.test_on_batch(val_x, val_y)
-                progbar.add(val_x.shape[0], values=[('test loss', score[0])])
-
-
-
+                # val_x, val_y = get_validation(args.batchsize)
+                # model.predict(val_x, val_y)                
+                # test_progbar = keras.utils.generic_utils.Progbar(val_x.shape[0])
+                # score = model.test_on_batch(val_x, val_y)
+                # progbar.add(val_x.shape[0], values=[('test loss', score[0])])
                 
 if __name__ == "__main__":
    args = build_parser().parse_args()
